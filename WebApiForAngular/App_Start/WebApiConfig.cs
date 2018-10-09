82cff29f48aef306e77e8049f965723c6eae6423
sanjureena <sanjureena143@gmail.com>
2018-10-09 18:35:31 +0530

    update

--- WebApiForAngular/App_Start/WebApiConfig.cs
+++ WebApiForAngular/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace WebApiForAngular
     {
         public static void Register(HttpConfiguration config)
         {
-            config.MessageHandlers.Add(new MethodOverrideHandler());
+            //config.MessageHandlers.Add(new MethodOverrideHandler());
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
 
@@ -54,26 +54,26 @@ namespace WebApiForAngular
         }
     }
 
-    public class MethodOverrideHandler : DelegatingHandler
-    {
-        readonly string[] _methods = { "DELETE", "HEAD", "PUT" };
-        const string _header = "X-HTTP-Method-Override";
+    //public class MethodOverrideHandler : DelegatingHandler
+    //{
+    //    readonly string[] _methods = { "DELETE", "HEAD", "PUT" };
+    //    const string _header = "X-HTTP-Method-Override";
 
-        protected override Task<HttpResponseMessage> SendAsync(
-            HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            // Check for HTTP POST with the X-HTTP-Method-Override header.
-            if (request.Method == HttpMethod.Post && request.Headers.Contains(_header))
-            {
-                // Check if the header value is in our methods list.
-                var method = request.Headers.GetValues(_header).FirstOrDefault();
-                if (_methods.Contains(method, StringComparer.InvariantCultureIgnoreCase))
-                {
-                    // Change the request method.
-                    request.Method = new HttpMethod(method);
-                }
-            }
-            return base.SendAsync(request, cancellationToken);
-        }
-    }
+    //    protected override Task<HttpResponseMessage> SendAsync(
+    //        HttpRequestMessage request, CancellationToken cancellationToken)
+    //    {
+    //        // Check for HTTP POST with the X-HTTP-Method-Override header.
+    //        if (request.Method == HttpMethod.Post && request.Headers.Contains(_header))
+    //        {
+    //            // Check if the header value is in our methods list.
+    //            var method = request.Headers.GetValues(_header).FirstOrDefault();
+    //            if (_methods.Contains(method, StringComparer.InvariantCultureIgnoreCase))
+    //            {
+    //                // Change the request method.
+    //                request.Method = new HttpMethod(method);
+    //            }
+    //        }
+    //        return base.SendAsync(request, cancellationToken);
+    //    }
+    //}
 }
